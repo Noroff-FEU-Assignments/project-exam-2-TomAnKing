6821eb262a815cdf42921049e9993da90edbd4f6
TomAnKing <71315446+TomAnKing@users.noreply.github.com>
2022-05-22 16:29:30 +0200

    post images with hotels

--- src/components/admin/AddHotel.js
+++ src/components/admin/AddHotel.js
@@ -4,6 +4,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import FormError from "../forms/FormError";
 import useAxios from "../../hooks/useAxios";
+import axios from "axios";
 
 const schema = yup.object().shape({
   title: yup.string().required("Title is required"),
@@ -30,19 +31,50 @@ export default function AddHotel() {
     setSubmitting(true);
     setServerError(null);
 
+    var formData = new FormData();
+    var formData2 = new FormData();
+    var formData3 = new FormData();
+
+    var inputs = document.querySelectorAll("input[type=file]");
+
+    formData.append("file", inputs[0].files[0]);
+    formData2.append("file", inputs[1].files[0]);
+    formData3.append("file", inputs[2].files[0]);
+
     data.status = "publish";
 
-    console.log(data);
-
     try {
-      const response = await http.post("wp/v2/hotels", data);
+      await http.post("/wp/v2/media", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      await http.post("/wp/v2/media", formData2, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      await http.post("/wp/v2/media", formData3, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
-      // console.log("response", response.data);
+      const images = await axios.get(
+        "https://holidaze.tomanking.one/wp-json/wp/v2/media"
+      );
+
+      data.fields.modal_image_2 = images.data[0].id;
+      data.fields.modal_image_1 = images.data[1].id;
+      data.fields.image = images.data[2].id;
+
+      await http.post("wp/v2/hotels", data);
     } catch (error) {
       console.log("error", error);
       setServerError(error.toString());
     } finally {
       setSubmitting(false);
+      window.location.reload(false);
     }
   }
 
@@ -94,10 +126,34 @@ export default function AddHotel() {
 
             <input
               name="stars"
-              {...register("fields.test_bilde")}
               className="formInput"
               type="file"
               accept="image/*"
+              id="file0"
+            />
+            {errors.stars && <FormError>{errors.stars.message}</FormError>}
+          </div>
+          <div>
+            <label className="formLabel">Image</label>
+
+            <input
+              name="stars"
+              className="formInput"
+              type="file"
+              accept="image/*"
+              id="file1"
+            />
+            {errors.stars && <FormError>{errors.stars.message}</FormError>}
+          </div>
+          <div>
+            <label className="formLabel">Image</label>
+
+            <input
+              name="stars"
+              className="formInput"
+              type="file"
+              accept="image/*"
+              id="file2"
             />
             {errors.stars && <FormError>{errors.stars.message}</FormError>}
           </div>
